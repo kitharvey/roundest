ed43d40c1104c4baf6eba981ea6844b0665605fe
Kit Harvey Caubalejo <kitharveycaubalejo@gmail.com>
2025-03-23 01:23:00 +0800

    fix multiple fetch

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,7 +5,7 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
 	if (event.platform?.env?.pokemon) {
 		const kv = event.platform.env.pokemon as KVNamespace;
-		event.locals.matchups = await getMatchups(kv, 3); // Fetch 3 for preloading
+		event.locals.matchups = await getMatchups(kv, 1); // Fetch 3 for preloading
 	}
 	const response = await resolve(event);
 	return response;
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,12 @@
-import { getMatchups } from '$lib/workers/getMatchups';
-import type { KVNamespace } from '@cloudflare/workers-types';
 import type { Actions, PageServerLoad } from './$types';
-import { error } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async ({ platform, locals }) => {
-	if (!platform?.env?.pokemon) {
-		throw error(500, 'KV namespace not found');
-	}
-
-	const kv = platform.env.pokemon as KVNamespace;
-	// Use locals.matchups if set by hooks, otherwise fetch directly
-	const matchups = locals.matchups?.length ? locals.matchups : await getMatchups(kv, 1);
+export const load: PageServerLoad = async ({ locals }) => {
+	const matchups = locals.matchups;
 	return { matchups };
 };
 
 export const actions: Actions = {
-	default: async ({ request, locals, platform }) => {
+	default: async ({ request }) => {
 		const data = await request.formData();
 		const winnerId = data.get('winner_id');
 		const pokemon1Id = data.get('pokemon1_id');
@@ -25,10 +16,5 @@ export const actions: Actions = {
 			const loserId = winnerId === pokemon1Id ? pokemon2Id : pokemon1Id;
 			console.log({ winnerId, loserId });
 		}
-		if (!platform?.env?.pokemon) {
-			throw error(500, 'KV namespace not found');
-		}
-		const kv = platform.env.pokemon as KVNamespace;
-		locals.matchups = await getMatchups(kv, 1);
 	}
 };
