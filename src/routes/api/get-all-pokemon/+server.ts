f32bca7da48498e3790d9d8c192c0c6e1e88ad04
Kit Harvey Caubalejo <kitharveycaubalejo@gmail.com>
2025-03-23 00:41:50 +0800

    fix worker limit, undo comment

--- src/routes/api/get-all-pokemon/+server.ts
+++ src/routes/api/get-all-pokemon/+server.ts
@@ -1,31 +1,31 @@
-// import { json, type RequestHandler } from '@sveltejs/kit';
-// import type { Pokemon } from '../../../app';
+import { json, type RequestHandler } from '@sveltejs/kit';
+import type { Pokemon } from '../../../app';
 
-// export const GET: RequestHandler = async ({ platform }) => {
-// 	try {
-// 		if (!platform?.env?.pokemon) {
-// 			return json({ error: 'KV namespace not found' }, { status: 500 });
-// 		}
-// 		const kv = platform.env.pokemon;
+export const GET: RequestHandler = async ({ platform }) => {
+	try {
+		if (!platform?.env?.pokemon) {
+			return json({ error: 'KV namespace not found' }, { status: 500 });
+		}
+		const kv = platform.env.pokemon;
 
-// 		const keys = await kv.list({ prefix: 'pokemon:' });
-// 		const pokemonPromises = keys.keys.map(async (key) => {
-// 			const pokemonString = await kv.get(key.name);
-// 			if (pokemonString) {
-// 				return JSON.parse(pokemonString) as Pokemon;
-// 			}
-// 			return null;
-// 		});
+		const keys = await kv.list({ prefix: 'pokemon:' });
+		const pokemonPromises = keys.keys.map(async (key) => {
+			const pokemonString = await kv.get(key.name);
+			if (pokemonString) {
+				return JSON.parse(pokemonString) as Pokemon;
+			}
+			return null;
+		});
 
-// 		const pokemonData = (await Promise.all(pokemonPromises)).filter(
-// 			(pokemon): pokemon is Pokemon => pokemon !== null
-// 		);
+		const pokemonData = (await Promise.all(pokemonPromises)).filter(
+			(pokemon): pokemon is Pokemon => pokemon !== null
+		);
 
-// 		pokemonData.sort((a, b) => a.id - b.id);
+		pokemonData.sort((a, b) => a.id - b.id);
 
-// 		return json({ pokemon: pokemonData, total: pokemonData.length });
-// 	} catch (error) {
-// 		console.error('Error fetching Pokemon data:', error);
-// 		return json({ error: 'Failed to fetch Pokemon data' }, { status: 500 });
-// 	}
-// };
+		return json({ pokemon: pokemonData, total: pokemonData.length });
+	} catch (error) {
+		console.error('Error fetching Pokemon data:', error);
+		return json({ error: 'Failed to fetch Pokemon data' }, { status: 500 });
+	}
+};
