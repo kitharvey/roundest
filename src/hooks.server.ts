04db5456580cfc339df3765e70cdd7286231066b
Kit Harvey Caubalejo <kitharveycaubalejo@gmail.com>
2025-03-22 23:57:54 +0800

    fixing worker limit

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,7 @@
 import type { Handle } from '@sveltejs/kit';
-import { getMatchups } from '$lib/workers/getMatchups';
-import type { KVNamespace } from '@cloudflare/workers-types';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.fetchMatchups = async () => {
-		if (event.platform?.env?.pokemon) {
-			const kv = event.platform.env.pokemon as KVNamespace;
-			const matchups = await getMatchups(kv, 1);
-			event.locals.matchups = matchups;
-			return matchups;
-		}
-		return [];
-	};
-	event.locals.matchups = await event.locals.fetchMatchups();
-
+	event.locals.matchups = null;
 	const response = await resolve(event);
 	return response;
 };
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,22 @@
+import { getMatchups } from '$lib/workers/getMatchups';
+import type { KVNamespace } from '@cloudflare/workers-types';
 import type { Actions, PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ platform, locals }) => {
+	if (!platform?.env?.pokemon) {
+		throw error(500, 'KV namespace not found');
+	}
+
+	const kv = platform.env.pokemon as KVNamespace;
+	locals.matchups = await getMatchups(kv, 1);
+
 	const matchups = locals.matchups;
 	return { matchups };
 };
 
 export const actions: Actions = {
-	default: async ({ request, locals }) => {
+	default: async ({ request, locals, platform }) => {
 		const data = await request.formData();
 		const winnerId = data.get('winner_id');
 		const pokemon1Id = data.get('pokemon1_id');
@@ -16,7 +26,10 @@ export const actions: Actions = {
 			const loserId = winnerId === pokemon1Id ? pokemon2Id : pokemon1Id;
 			console.log({ winnerId, loserId });
 		}
-
-		await locals.fetchMatchups();
+		if (!platform?.env?.pokemon) {
+			throw error(500, 'KV namespace not found');
+		}
+		const kv = platform.env.pokemon as KVNamespace;
+		locals.matchups = await getMatchups(kv, 1);
 	}
 };
