dc90edde44e4e0cb9eece52c48872fbcffe19429
Kit Harvey Caubalejo <kitharveycaubalejo@gmail.com>
2025-03-24 21:34:00 +0800

    remove image transform

--- src/lib/components/app/Image.svelte
+++ src/lib/components/app/Image.svelte
@@ -9,43 +9,20 @@
 		alt: string;
 		class?: string;
 	}>();
-
-	const baseURL = '/cdn-cgi/image/';
-
-	let optimizedDefaultUrl = $derived(
-		import.meta.env.DEV
-			? src
-			: `${baseURL}${[`width=96`, `height=96`, 'quality=70', 'format=webp']
-					.filter(Boolean)
-					.join(',')}/${encodeURIComponent(src)}`
-	);
-
-	let srcset = $derived(
-		import.meta.env.DEV
-			? ''
-			: `${baseURL}width=96,height=96,quality=70,format=webp/${encodeURIComponent(src)} 96w`
-	);
-
-	function handleError(event: Event) {
-		const img = event.target as HTMLImageElement;
-		img.src = src;
-	}
 </script>
 
-<picture>
-	{#if !import.meta.env.DEV && srcset}
-		<source type="image/webp" {srcset} sizes="96px" />
-	{/if}
-	<img
-		src={optimizedDefaultUrl}
-		{alt}
-		class={className}
-		loading="eager"
-		decoding="auto"
-		{...rest}
-		onerror={handleError}
-	/>
-</picture>
+<img
+	{src}
+	{alt}
+	class={className}
+	loading="eager"
+	decoding="auto"
+	{...rest}
+	fetchpriority="high"
+	crossorigin="anonymous"
+	referrerpolicy="no-referrer"
+	cache-control="max-age=31536000"
+/>
 
 <style>
 	img {
