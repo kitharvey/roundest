a0981ee1a29bd518151e98fc528b6f8f35801728
Kit Harvey Caubalejo <kitharveycaubalejo@gmail.com>
2025-05-03 14:53:06 +0800

    rename constants

---
+++ src/lib/helpers/escapeXml.ts
@@ -0,0 +1,19 @@
+export function escapeXml(unsafe: string): string {
+	if (!unsafe) return '';
+	return unsafe.replace(/[<>&'"]/g, (c) => {
+		switch (c) {
+			case '<':
+				return '&lt;';
+			case '>':
+				return '&gt;';
+			case '&':
+				return '&amp;';
+			case "'":
+				return '&apos;';
+			case '"':
+				return '&quot;';
+			default:
+				return c;
+		}
+	});
+}
